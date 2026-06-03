cf1688732bbc1507d9ab3b117ad9b0a441d9aa7f
OwiesAlhajali <116441812+OwiesAlhajali@users.noreply.github.com>
2026-06-03 16:59:30 +0300

    Test

--- app/src/main/java/com/bibliotech/app/ui/theme/Color.kt
+++ app/src/main/java/com/bibliotech/app/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Navy = Color(0xFF0F172A)
+val Gold = Color(0xFFF59E0B)
+val SoftGray = Color(0xFF94A3B8)
+val White = Color(0xFFFFFFFF)
